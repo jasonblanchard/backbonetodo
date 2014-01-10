f5d700b2c36a0cce5a92da89fbf7475c05398cad
Jason Blanchard <jas.blanchard@gmail.com>
2014-01-10 08:24:38 -0500

    first commit

---
+++ app/assets/javascripts/backbonetodo.js
@@ -0,0 +1,13 @@
+window.Backbonetodo = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Backbonetodo.initialize();
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbonetodo::Application.config.secret_token = '9e51d22fc73403317f605b1c9058d8ae89774f78c1be44f4728528a81092cebbb101f43a28b378033abff4a77f0f331bb20dc607f51608ec6e44888c569f2c67'
