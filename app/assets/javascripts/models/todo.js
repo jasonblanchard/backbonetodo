08bf4dc2764ab878a04756d4114445879f61b402
Jason Blanchard <jas.blanchard@gmail.com>
2014-01-18 10:13:26 -0500

    Added toggle status function

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -3,6 +3,15 @@ Backbonetodo.Models.Todo = Backbone.Model.extend({
   defaults: {
     title: 'Something to do...',
     complete: false
+  },
+
+  toggleStatus: function() {
+    if (this.get('complete') == false) {
+      this.set({'complete': true});
+    } else {
+      this.set({'complete': false});
+    }
+    this.save();
   }
 
 
--- app/assets/javascripts/views/todos/todo.js
+++ app/assets/javascripts/views/todos/todo.js
@@ -6,10 +6,18 @@ Backbonetodo.Views.Todo = Backbone.View.extend({
     this.model.on('hide', this.remove, this);
     this.model.on('change', this.render, this);
   },
+  
+  events: {
+    'click': 'toggleStatus'
+  },
 
   render: function() {
     var attributes = this.model.toJSON();
     this.$el.html(this.template(attributes));
     return this;
-  }
+  },
+
+  toggleStatus: function() {
+    this.model.toggleStatus();
+   }
 });
